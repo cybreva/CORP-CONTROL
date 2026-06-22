3839b9acac8bff15655ca39929964302784370a7
Tanmay <mtvkhanna@gmail.com>
2026-06-22 15:17:31 +0530

    formed a struct

---
+++ admincalc++.cpp
@@ -0,0 +1,20 @@
+#include <iostream> 
+
+using namespace std;
+
+struct Settings
+{
+    bool addition;
+    bool subtraction;
+    bool multiplication;
+    bool division;
+};
+
+
+
+int main()
+{
+    struct settings s ={false,false,false,false} ;
+
+
+}
